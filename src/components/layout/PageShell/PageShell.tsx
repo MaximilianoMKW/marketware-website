195d1c1ba16131961608ec2201b1c8e9629dbaa0
Max Lopes <mlopes@marketware.eu>
2026-05-20 11:03:28 +0100

    Fix mobile carousel and route scroll

--- src/components/layout/PageShell/PageShell.tsx
+++ src/components/layout/PageShell/PageShell.tsx
@@ -1,8 +1,23 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import { Footer } from '../Footer/Footer';
 import { Header } from '../Header/Header';
 
 export function PageShell() {
+  const { hash, pathname } = useLocation();
+
+  useEffect(() => {
+    if (hash) {
+      const id = hash.slice(1);
+      window.requestAnimationFrame(() => {
+        document.getElementById(id)?.scrollIntoView({ block: 'start' });
+      });
+      return;
+    }
+
+    window.scrollTo({ top: 0, left: 0 });
+  }, [hash, pathname]);
+
   return (
     <>
       <Header />
